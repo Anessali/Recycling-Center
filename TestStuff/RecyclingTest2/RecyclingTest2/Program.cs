55e474b2af29852500aa39b7ce70744dc88ea73c
Preston Hunt <ph0859241@otc.edu>
2018-10-03 17:26:44 -0500

    This is a test program for messing with.
    This is a test do what you want with it.

---
+++ TestStuff/RecyclingTest2/RecyclingTest2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RecyclingTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i > 0; i++)
+            {
+                Console.Write("This is a test.");
+                Console.WriteLine(" ");
+
+            }
+        }
+    }
+}
